a2669e65129574cda9f9964c90876612fcb87c3e
CA Meijer <hammingweight@gmail.com>
2014-02-16 21:27:52 +0200

    Stub Java project.

---
+++ zookeeperl-java/src/com/hammingweight/zookeeperl/ZooKeeperNode.java
@@ -0,0 +1,13 @@
+package com.hammingweight.zookeeperl;
+
+public class ZooKeeperNode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
