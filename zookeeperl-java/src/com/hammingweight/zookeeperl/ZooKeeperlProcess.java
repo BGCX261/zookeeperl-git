ef0345987c1e63a10c6fd2fde4106f9d03292215
CA Meijer <hammingweight@gmail.com>
2014-03-22 22:39:51 +0200

    Some minimal node creation code.

--- zookeeperl-java/src/com/hammingweight/zookeeperl/ZooKeeperlProcess.java
+++ zookeeperl-java/src/com/hammingweight/zookeeperl/ZooKeeperlProcess.java
@@ -1,5 +1,7 @@
 package com.hammingweight.zookeeperl;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
 
 import com.ericsson.otp.erlang.OtpErlangAtom;
@@ -7,7 +9,6 @@ import com.ericsson.otp.erlang.OtpErlangObject;
 import com.ericsson.otp.erlang.OtpErlangPid;
 import com.ericsson.otp.erlang.OtpErlangTuple;
 import com.ericsson.otp.erlang.OtpMbox;
-import com.ericsson.otp.erlang.OtpOutputStream;
 
 public class ZooKeeperlProcess implements Runnable {
 
@@ -21,8 +22,12 @@ public class ZooKeeperlProcess implements Runnable {
 	}
 	
 	private void processHeartbeat(OtpErlangPid pid, OtpErlangObject uid) {
-		OtpErlangTuple resp = new OtpErlangTuple(new OtpErlangObject[] {uid, new OtpErlangTuple(new OtpErlangAtom("heartbeat_response"))});
+		OtpErlangTuple resp = new OtpErlangTuple(new OtpErlangObject[] {uid, new OtpErlangTuple(new OtpErlangAtom("heartbeat"))});
 		this.mbox.send(pid, resp);
+	}
+	
+	private void processCreateSync(OtpErlangPid pid, OtpErlangObject uid, OtpErlangTuple msgBody) throws Throwable {
+		this.zookeeper.create("/foo", new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
 	}
 	
 	void processNextMessage() {
@@ -33,6 +38,9 @@ public class ZooKeeperlProcess implements Runnable {
 			OtpErlangTuple msgBody = (OtpErlangTuple) msg.elementAt(2);
 			if (msgBody.elementAt(0).equals(new OtpErlangAtom("heartbeat"))) {
 				processHeartbeat(pid, uid);
+			}
+			else if (msgBody.elementAt(0).equals(new OtpErlangAtom("create_sync"))) {
+				processCreateSync(pid, uid, msgBody);
 			}
 		} catch (Throwable t) {
 			// TODO Auto-generated catch block
--- zookeeperl-java/test/com/hammingweight/zookeeperl/ZooKeeperlProcessTest.java
+++ zookeeperl-java/test/com/hammingweight/zookeeperl/ZooKeeperlProcessTest.java
@@ -2,13 +2,18 @@ package com.hammingweight.zookeeperl;
 
 import static org.junit.Assert.*;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.ACL;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
 import com.ericsson.otp.erlang.OtpErlangAtom;
+import com.ericsson.otp.erlang.OtpErlangBinary;
 import com.ericsson.otp.erlang.OtpErlangObject;
 import com.ericsson.otp.erlang.OtpErlangPid;
+import com.ericsson.otp.erlang.OtpErlangString;
 import com.ericsson.otp.erlang.OtpErlangTuple;
 import com.ericsson.otp.erlang.OtpMbox;
 
@@ -47,7 +52,7 @@ public class ZooKeeperlProcessTest {
 		verify(mbox).receive();
 		
 		// We expect the process to send back a heartbeat_response
-		OtpErlangAtom respType = new OtpErlangAtom("heartbeat_response");
+		OtpErlangAtom respType = new OtpErlangAtom("heartbeat");
 		OtpErlangTuple respBody = new OtpErlangTuple(respType);
 	
 		ArgumentCaptor<OtpErlangTuple> arg = ArgumentCaptor.forClass(OtpErlangTuple.class);
@@ -58,4 +63,29 @@ public class ZooKeeperlProcessTest {
 		assertEquals(2, resp.arity());
 	}
 
+	@Test
+	public void testCreateSyncEphemeral() throws Throwable {
+		ZooKeeper zookeeper = mock(ZooKeeper.class);
+		OtpMbox mbox = mock(OtpMbox.class);
+		ZooKeeperlProcess zkProcess = new ZooKeeperlProcess(zookeeper, mbox);
+		
+		// Expect a create message
+		OtpErlangPid pid = mock(OtpErlangPid.class);
+		OtpErlangAtom uid = new OtpErlangAtom("uid");
+		OtpErlangAtom msgType = new OtpErlangAtom("create_sync");
+		OtpErlangString path = new OtpErlangString("/foo");
+		OtpErlangBinary data = new OtpErlangBinary(new byte[7]);
+		OtpErlangAtom createMode = new OtpErlangAtom("ephemeral");
+		OtpErlangTuple msgBody = new OtpErlangTuple(new OtpErlangObject[]{msgType, path, data, createMode});
+		OtpErlangTuple msg = new OtpErlangTuple(new OtpErlangObject[]{pid, uid, msgBody});
+		
+		when(mbox.receive()).thenReturn(msg);
+
+		zkProcess.processNextMessage();
+		
+		verify(mbox).receive();
+
+		// We expect the ZooKeeper process to receive a create message
+		verify(zookeeper).create(any(String.class), any(byte[].class), eq(Ids.OPEN_ACL_UNSAFE), eq(CreateMode.EPHEMERAL));
+	}
 }
